bf0921aa392d9b2f0117ccd8d96331c8059c0f3d
Maxim Kuznetsov <d1sma@yandex.ru>
2023-08-03 22:39:58 +0300

    Memory storage

--- internal/client/storage/memory/storage.go
+++ internal/client/storage/memory/storage.go
@@ -1,37 +1,102 @@
 package memory
 
 import (
+	"sync"
+
+	"github.com/MaxReX92/go-yandex-gophkeeper/internal/client/storage"
 	"github.com/MaxReX92/go-yandex-gophkeeper/internal/model"
+	"github.com/MaxReX92/go-yandex-gophkeeper/pkg/logger"
 )
 
 type memoryStorage struct {
+	secrets map[model.SecretType]map[string]model.Secret
+	lock    sync.RWMutex
 }
 
 func NewStorage() *memoryStorage {
-	return &memoryStorage{}
+	return &memoryStorage{
+		secrets: make(map[model.SecretType]map[string]model.Secret),
+		lock:    sync.RWMutex{},
+	}
 }
 
 func (m *memoryStorage) AddSecret(secret model.Secret) error {
-	//TODO implement me
-	panic("implement me")
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	secretsByType := m.ensureSecrets(secret.GetType())
+	identity := secret.GetIdentity()
+	_, ok := secretsByType[identity]
+	if ok {
+		return logger.WrapError("add new secret", storage.ErrSecretAlreadyExist)
+	}
+
+	secretsByType[identity] = secret
+	return nil
 }
 
 func (m *memoryStorage) ChangeSecret(secret model.Secret) error {
-	//TODO implement me
-	panic("implement me")
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	secretsByType := m.ensureSecrets(secret.GetType())
+	identity := secret.GetIdentity()
+	_, ok := secretsByType[identity]
+	if !ok {
+		return logger.WrapError("change secret", storage.ErrSecretNotFound)
+	}
+
+	secretsByType[identity] = secret
+	return nil
 }
 
 func (m *memoryStorage) GetSecretById(secretType model.SecretType, identity string) (model.Secret, error) {
-	//TODO implement me
-	panic("implement me")
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	secretsByType := m.ensureSecrets(secretType)
+	secret, ok := secretsByType[identity]
+	if !ok {
+		return nil, logger.WrapError("get secret", storage.ErrSecretNotFound)
+	}
+
+	return secret, nil
 }
 
 func (m *memoryStorage) GetAllSecrets(secretType model.SecretType) ([]model.Secret, error) {
-	//TODO implement me
-	panic("implement me")
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	secretsByType := m.ensureSecrets(secretType)
+	secretsLen := len(secretsByType)
+	result := make([]model.Secret, secretsLen)
+
+	i := 0
+	for _, secret := range secretsByType {
+		result[i] = secret
+		i++
+	}
+
+	return result, nil
 }
 
 func (m *memoryStorage) RemoveSecret(secret model.Secret) error {
-	//TODO implement me
-	panic("implement me")
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	secretsByType := m.ensureSecrets(secret.GetType())
+	identity := secret.GetIdentity()
+	delete(secretsByType, identity)
+
+	return nil
+}
+
+func (m *memoryStorage) ensureSecrets(secretType model.SecretType) map[string]model.Secret {
+	secretsByType, ok := m.secrets[secretType]
+	if !ok {
+		secretsByType = make(map[string]model.Secret)
+		m.secrets[secretType] = secretsByType
+	}
+
+	return secretsByType
 }

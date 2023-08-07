812343567c27f4bdd6f4953ff173ed384682f257
Maxim Kuznetsov <d1sma@yandex.ru>
2023-08-08 01:48:17 +0300

    Random generator

--- internal/client/generator/rand/randomGenerator.go
+++ internal/client/generator/rand/randomGenerator.go
@@ -1,13 +1,27 @@
 package rand
 
-type randomGenerator struct {
+import "math/rand"
+
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+type RandomGeneratorConfig interface {
+	IdentityLen() int32
 }
 
-func NewGenerator() *randomGenerator {
-	return &randomGenerator{}
+type randomGenerator struct {
+	identityLen int32
+}
+
+func NewGenerator(conf RandomGeneratorConfig) *randomGenerator {
+	return &randomGenerator{
+		identityLen: conf.IdentityLen(),
+	}
 }
 
 func (r *randomGenerator) GenerateNewIdentity() string {
-	//TODO implement me
-	panic("implement me")
+	identity := make([]byte, r.identityLen)
+	for i := range identity {
+		identity[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(identity)
 }
